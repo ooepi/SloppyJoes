16bf25aee3b2d27d9de3b1e0093af87a7194e7b5
ooepi <robert.harmaakivi@gmail.com>
2021-01-20 23:18:00 +0200

    Finished the code to make the random menu

--- SloppyJoes/MainWindow.xaml.cs
+++ SloppyJoes/MainWindow.xaml.cs
@@ -23,7 +23,49 @@ namespace SloppyJoes
         public MainWindow()
         {
             InitializeComponent();
+            MakeTheMenu();
         }
 
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i<3)
+                {
+                    menuItems[i] = new MenuItem();
+                }
+                else
+                {
+                    menuItems[i] = new MenuItem()
+                    {
+                        Breads = new string[] { "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel" }
+                    };
+                }
+            }
+
+            SandwichOne.Text = menuItems[0].GenerateMenuItem();
+            PriceOne.Text = menuItems[0].RandomPrice();
+            SandwichTwo.Text = menuItems[0].GenerateMenuItem();
+            PriceTwo.Text = menuItems[0].RandomPrice();
+            SandwichThree.Text = menuItems[0].GenerateMenuItem();
+            PriceThree.Text = menuItems[0].RandomPrice();
+            SandwichFour.Text = menuItems[0].GenerateMenuItem();
+            PriceFour.Text = menuItems[0].RandomPrice();
+            SandwichFive.Text = menuItems[0].GenerateMenuItem();
+            PriceFive.Text = menuItems[0].RandomPrice();
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },
+                Breads = new string[] { "a gluten free roll", "a wrap", "pita" },
+                Condiments = new string[] { "dijon mustard", "miso dressing", "au jus" }
+            };
+
+            SandwichSix.Text = specialMenuItem.GenerateMenuItem();
+            PriceSix.Text = specialMenuItem.RandomPrice();
+
+            guacamole.Text = "Add guacamole for " + specialMenuItem.RandomPrice();
+        }
     }
 }
